9887dac046e5fa56e5fe63f9a4a6845639fb120c
rkaufman <rkaufman@student.42wolfsburg.de>
2022-05-30 20:59:58 +0200

    ex02 done going to Linux testing

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -3,17 +3,109 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkaufman <rkaufman@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*   By: rkaufman <rkaufman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 22:06:32 by rkaufman          #+#    #+#             */
-/*   Updated: 2022/05/24 22:07:00 by rkaufman         ###   ########.fr       */
+/*   Updated: 2022/05/30 19:46:03 by rkaufman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
+# include <iostream>
+# include <stdexcept>
+# include "colors.h"
 
+template <typename T>
+class Array
+{
+	public:
 
+		Array(void);
+		Array(unsigned int size);
+		Array(Array const & obj);
+
+		Array & operator=(Array<T> const & obj);
+		T & operator[](unsigned int index);
+
+		unsigned int	size(void);
+
+		~Array(void);
+
+	protected:
+
+	private:
+	
+		T				* _array;
+		unsigned int	_size;
+	
+};
+
+template <typename T>
+Array<T>::Array(void)
+{
+	if (DEBUG)
+		std::cout << COLOR_MAGENTA << "[Array] default constructor called." << std::endl << COLOR_DEFAULT;
+	_size = 0;
+	_array = NULL;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int size)
+{
+	if (DEBUG)
+		std::cout << COLOR_MAGENTA << "[Array] parameter constructor called." << std::endl << COLOR_DEFAULT;
+	_array = new T[size];
+	_size = size;
+}
+
+template <typename T>
+Array<T>::Array(Array<T> const & obj)
+{
+	if (DEBUG)
+		std::cout << COLOR_MAGENTA << "[Array] copy constructor called." << std::endl << COLOR_DEFAULT;
+	this->_size = 0;
+	*this = obj;
+}
+
+template <typename T>
+Array<T> & Array<T>::operator=(Array<T> const & obj)
+{
+	if (DEBUG)
+		std::cout << COLOR_MAGENTA << "[Array] assignement constructor called." << std::endl << COLOR_DEFAULT;
+	if (this == &obj)
+		return (*this);
+	if (this->_size > 0)
+		delete [] this->_array;
+	this->_size = obj._size;
+	this->_array = new T[this->_size];
+	for (unsigned int i = 0; i < this->_size; i++)
+		this->_array[i] = obj._array[i];
+	return (*this);
+}
+
+template <typename T>
+T & Array<T>::operator[](unsigned int index)
+{
+	if (index >= this->_size)
+		throw std::out_of_range("Array::Index out of bounds!");
+	return (this->_array[index]);
+}
+
+template <typename T>
+unsigned int	Array<T>::size(void)
+{
+	return (this->_size);
+}
+
+template <typename T>
+Array<T>::~Array(void)
+{
+	if (DEBUG)
+		std::cout << COLOR_MAGENTA << "[Array] deconstructor called." << std::endl << COLOR_DEFAULT;
+	if (this->_size > 0)
+		delete [] this->_array;
+}
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,22 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkaufman <rkaufman@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*   By: rkaufman <rkaufman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 22:06:30 by rkaufman          #+#    #+#             */
-/*   Updated: 2022/05/24 22:14:37 by rkaufman         ###   ########.fr       */
+/*   Updated: 2022/05/30 19:46:49 by rkaufman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <Array.hpp>
+#include "Array.hpp"
 
 #define MAX_VAL 750
+
+static void	print_text(std::string output);
+
 int main(int, char**)
 {
-	Array<int> numbers(MAX_VAL);
-	int* mirror = new int[MAX_VAL];
+	print_text("Creating two Arrays.");
+	Array<int>	numbers(MAX_VAL);
+	Array<int>	empty_array;
+	int			* mirror = new int[MAX_VAL];
 
+	std::cout << "Size of empty Array: " << empty_array.size() << std::endl;
+	try
+	{
+		std::cout << "Accessing empty Array: " << empty_array[0] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << COLOR_RED << e.what() << '\n' << COLOR_DEFAULT;
+	}
+	
+	std::cout << "\nSize of big Array: " << numbers.size() << std::endl;
+	std::cout << "value of Array[5]: " << numbers[5] << std::endl;
+
+	print_text("Assigning random numbers to Array.");
 	srand(time(NULL));
 	for (int i = 0; i < MAX_VAL; i++)
 	{
@@ -26,13 +45,15 @@ int main(int, char**)
 		numbers[i] = value;
 		mirror[i] = value;
 	}
-
+	
+	print_text("Testing with scope of Variables and calling copy constructor.");
 	//SCOPE
 	{
 		Array<int> tmp = numbers;
 		Array<int> test(tmp);
 	}
 
+	print_text("Comparing values.");
 	for (int i = 0; i < MAX_VAL; i++)
 	{
 		if (mirror[i] != numbers[i])
@@ -42,29 +63,38 @@ int main(int, char**)
 		}
 	}
 
+	print_text("Try to access index out of bounds - negativ.");
 	try
 	{
 		numbers[-2] = 0;
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << COLOR_RED << e.what() << '\n' << COLOR_DEFAULT;
 	}
-
+	
+	print_text("Try to access index out of bounds - greater max.");
 	try
 	{
 		numbers[MAX_VAL] = 0;
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << COLOR_RED << e.what() << '\n' << COLOR_DEFAULT;
 	}
 
+	print_text("Assigning other random numbers for Array.");
 	for (int i = 0; i < MAX_VAL; i++)
 	{
 		numbers[i] = rand();
 	}
 
+	print_text("Deleting Arrays.");
 	delete [] mirror;
 	return (0);
+}
+
+static void	print_text(std::string output)
+{
+	std::cout << COLOR_YELLOW << "\n" << output << "\n\n" << COLOR_DEFAULT;
 }
